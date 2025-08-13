0f3c9859fcc6b63fcc360e003e9fc10b36d5011d
George Panaretos <georgepanaretos.cac@gmail.com>
2025-08-13 18:47:16 +0300

    Init Credit Checker service

---
+++ credit-checker-service/src/main/java/com/kafka/credit_checker_service/CreditCheckerServiceApplication.java
@@ -0,0 +1,13 @@
+package src.main.java.com.kafka.credit_checker_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreditCheckerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreditCheckerServiceApplication.class, args);
+	}
+
+}
---
+++ credit-checker-service/src/test/java/com/kafka/credit_checker_service/CreditCheckerServiceApplicationTests.java
@@ -0,0 +1,4 @@
+package com.kafka.credit_checker_service;
+
+class CreditCheckerServiceApplicationTests {
+}
